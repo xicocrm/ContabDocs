9b350abb06dbc8c62819d9487bbc069678d766dc
xicocrm <xicocrm@gmail.com>
2026-03-25 20:25:16 -0300

    Create Customer model interface

---
+++ packages/server/src/models/Customer.ts
@@ -0,0 +1,9 @@
+// TypeScript model for Customer
+export interface Customer {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
